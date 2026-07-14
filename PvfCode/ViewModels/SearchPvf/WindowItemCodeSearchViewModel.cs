27f49df7b8bfa4c496bb6c592b55bc8b8af20eaa
Lancarus <duzhen92@qq.com>
2026-07-14 14:32:26 +0800

    fix: restore item code search internals

--- PvfCode/ViewModels/SearchPvf/WindowItemCodeSearchViewModel.cs
+++ PvfCode/ViewModels/SearchPvf/WindowItemCodeSearchViewModel.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Collections.Pooled;
 using DevExpress.Mvvm;
@@ -20,64 +19,7 @@ namespace PvfCode.ViewModels.SearchPvf;
 
 public class WindowItemCodeSearchViewModel : ViewModelBase
 {
-	[CompilerGenerated]
-	private sealed class _003C_003Ec__DisplayClass35_0
-	{
-		public string BDZqqHaBUj;
-
-		public WindowItemCodeSearchViewModel VDnqdMiW0f;
-
-		public IEnumerable<string> VB3qeVMfMZ;
-
-		public _003C_003Ec__DisplayClass35_0()
-		{
-		}
-
-		internal IEnumerable<string> PVAqLQiimQ()
-		{
-			return AppCore.ViewModelBase.PVF.ListFileTable.ItemCodesToFilePathsAsync(BDZqqHaBUj, VDnqdMiW0f.SelectedItem.Value.Key);
-		}
-
-		internal void KK8qnhpBTP()
-		{
-			ConcurrentBag<ItemCodeSearchResult> concurrentBag = new ConcurrentBag<ItemCodeSearchResult>();
-			foreach (string item in VB3qeVMfMZ)
-			{
-				concurrentBag.Add(new ItemCodeSearchResult(item));
-			}
-			VDnqdMiW0f.SearchResult.AddRange(concurrentBag.ToArray());
-			VDnqdMiW0f.Count = ((VDnqdMiW0f.SearchResult != null) ? VDnqdMiW0f.SearchResult.Count : 0);
-		}
-	}
-
-	[CompilerGenerated]
-	private sealed class _003C_003Ec__DisplayClass38_0
-	{
-		public string H20qbu03iY;
-
-		public _003C_003Ec__DisplayClass38_0()
-		{
-		}
-
-		internal void EZcqtZKbg6(string it)
-		{
-			AppCore.ShowExtractLstWindow(H20qbu03iY);
-		}
-	}
-
-	[CompilerGenerated]
-	private SearchItemCodeConfig buhWqqSDe1;
-
-	[CompilerGenerated]
-	private TextDocument eRVWdrbN70;
-
-	[CompilerGenerated]
-	private IHighlightingDefinition bjDWe382L3;
-
-	private ConcurrentObservableCollection<ItemCodeSearchResult> RN8WtIdonR;
-
-	[CompilerGenerated]
-	private List<ItemCodeSearchResult> y9HWbB1nt9;
+	private ConcurrentObservableCollection<ItemCodeSearchResult> searchResult;
 
 	public bool RemoveDuplicate
 	{
@@ -87,23 +29,11 @@ public class WindowItemCodeSearchViewModel : ViewModelBase
 		}
 		set
 		{
-			SetProperty(() => RemoveDuplicate, value, aQcWLP0GAA);
+			SetProperty(() => RemoveDuplicate, value, OnRemoveDuplicateChanged);
 		}
 	}
 
-	public SearchItemCodeConfig Config
-	{
-		[CompilerGenerated]
-		get
-		{
-			return buhWqqSDe1;
-		}
-		[CompilerGenerated]
-		set
-		{
-			buhWqqSDe1 = value;
-		}
-	}
+	public SearchItemCodeConfig Config { get; set; }
 
 	public KeyValuePair<string, Dictionary<int, LstItem>>? SelectedItem
 	{
@@ -129,33 +59,9 @@ public class WindowItemCodeSearchViewModel : ViewModelBase
 		}
 	}
 
-	public TextDocument Document
-	{
-		[CompilerGenerated]
-		get
-		{
-			return eRVWdrbN70;
-		}
-		[CompilerGenerated]
-		set
-		{
-			eRVWdrbN70 = value;
-		}
-	}
+	public TextDocument Document { get; set; }
 
-	public IHighlightingDefinition Highlighting
-	{
-		[CompilerGenerated]
-		get
-		{
-			return bjDWe382L3;
-		}
-		[CompilerGenerated]
-		set
-		{
-			bjDWe382L3 = value;
-		}
-	}
+	public IHighlightingDefinition Highlighting { get; set; }
 
 	public int Count
 	{
@@ -173,34 +79,22 @@ public class WindowItemCodeSearchViewModel : ViewModelBase
 	{
 		get
 		{
-			if (RN8WtIdonR == null)
+			if (searchResult == null)
 			{
-				RN8WtIdonR = new ConcurrentObservableCollection<ItemCodeSearchResult>();
+				searchResult = new ConcurrentObservableCollection<ItemCodeSearchResult>();
 			}
-			return RN8WtIdonR;
+			return searchResult;
 		}
 		set
 		{
-			RN8WtIdonR = value;
+			searchResult = value;
 			RaisePropertyChanged("SearchResult");
 		}
 	}
 
-	public List<ItemCodeSearchResult> SelectedItems
-	{
-		[CompilerGenerated]
-		get
-		{
-			return y9HWbB1nt9;
-		}
-		[CompilerGenerated]
-		set
-		{
-			y9HWbB1nt9 = value;
-		}
-	}
+	public List<ItemCodeSearchResult> SelectedItems { get; set; }
 
-	private void aQcWLP0GAA()
+	private void OnRemoveDuplicateChanged()
 	{
 		Search();
 	}
@@ -214,10 +108,10 @@ public class WindowItemCodeSearchViewModel : ViewModelBase
 		{
 			Text = ""
 		};
-		Document.TextChanged += kJMWnI8gvd;
+		Document.TextChanged += OnDocumentTextChanged;
 	}
 
-	private void kJMWnI8gvd(object? sender, EventArgs P_1)
+	private void OnDocumentTextChanged(object? sender, EventArgs e)
 	{
 		Search();
 	}
@@ -225,8 +119,6 @@ public class WindowItemCodeSearchViewModel : ViewModelBase
 	[Command]
 	public async void Search()
 	{
-		_003C_003Ec__DisplayClass35_0 CS_0024_003C_003E8__locals13 = new _003C_003Ec__DisplayClass35_0();
-		CS_0024_003C_003E8__locals13.VDnqdMiW0f = this;
 		if (!SelectedItem.HasValue)
 		{
 			return;
@@ -234,23 +126,23 @@ public class WindowItemCodeSearchViewModel : ViewModelBase
 		SearchResult = null;
 		SearchResult = new ConcurrentObservableCollection<ItemCodeSearchResult>();
 		IsLoading = true;
-		CS_0024_003C_003E8__locals13.BDZqqHaBUj = Document.Text;
-		CS_0024_003C_003E8__locals13.VB3qeVMfMZ = await Task.Run(() => AppCore.ViewModelBase.PVF.ListFileTable.ItemCodesToFilePathsAsync(CS_0024_003C_003E8__locals13.BDZqqHaBUj, CS_0024_003C_003E8__locals13.VDnqdMiW0f.SelectedItem.Value.Key));
-		if (CS_0024_003C_003E8__locals13.VB3qeVMfMZ != null)
+		string itemCodes = Document.Text;
+		IEnumerable<string> filePaths = await Task.Run(() => AppCore.ViewModelBase.PVF.ListFileTable.ItemCodesToFilePathsAsync(itemCodes, SelectedItem.Value.Key));
+		if (filePaths != null)
 		{
 			if (RemoveDuplicate)
 			{
-				CS_0024_003C_003E8__locals13.VB3qeVMfMZ = CS_0024_003C_003E8__locals13.VB3qeVMfMZ.ToHashSet();
+				filePaths = filePaths.ToHashSet();
 			}
 			await Task.Run(delegate
 			{
-				ConcurrentBag<ItemCodeSearchResult> concurrentBag = new ConcurrentBag<ItemCodeSearchResult>();
-				foreach (string item in CS_0024_003C_003E8__locals13.VB3qeVMfMZ)
+				ConcurrentBag<ItemCodeSearchResult> results = new ConcurrentBag<ItemCodeSearchResult>();
+				foreach (string filePath in filePaths)
 				{
-					concurrentBag.Add(new ItemCodeSearchResult(item));
+					results.Add(new ItemCodeSearchResult(filePath));
 				}
-				CS_0024_003C_003E8__locals13.VDnqdMiW0f.SearchResult.AddRange(concurrentBag.ToArray());
-				CS_0024_003C_003E8__locals13.VDnqdMiW0f.Count = ((CS_0024_003C_003E8__locals13.VDnqdMiW0f.SearchResult != null) ? CS_0024_003C_003E8__locals13.VDnqdMiW0f.SearchResult.Count : 0);
+				SearchResult.AddRange(results.ToArray());
+				Count = ((SearchResult != null) ? SearchResult.Count : 0);
 			});
 		}
 		IsLoading = false;
@@ -281,24 +173,23 @@ public class WindowItemCodeSearchViewModel : ViewModelBase
 	[Command]
 	public async void ExtractSelectedItemsToLst()
 	{
-		_003C_003Ec__DisplayClass38_0 CS_0024_003C_003E8__locals4 = new _003C_003Ec__DisplayClass38_0();
 		if (SelectedItems == null)
 		{
 			return;
 		}
 		IEnumerable<string> files = SelectedItems.Select((ItemCodeSearchResult it) => it.FullPath);
-		CS_0024_003C_003E8__locals4.H20qbu03iY = ServiceItemCodeTable.FilesToLstItemsToString(AppCore.ViewModelBase.PVF, files, out var count);
-		if (string.IsNullOrEmpty(CS_0024_003C_003E8__locals4.H20qbu03iY))
+		string lstText = ServiceItemCodeTable.FilesToLstItemsToString(AppCore.ViewModelBase.PVF, files, out var count);
+		if (string.IsNullOrEmpty(lstText))
 		{
 			AppCore.ShowMsg(AppSetting.Instance.GetIlogger()?.GetStr("mess_NoFileCanBeExtractedToLst"), isError: true);
 			return;
 		}
-		AppCore.CopyString(CS_0024_003C_003E8__locals4.H20qbu03iY);
-		string text = string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_ExtractedLstCount"), count);
-		AppCore.Logger.Success(text);
-		await AppCore.Logger.ShowNotification(new NotificationViewModel<string>(AppSetting.Instance.AppName, text, Res.Instance.VisualStudioBlendLogo2015Pre_16x, AppSetting.Instance.GetIlogger()?.GetStr("mess_ViewDetails"), new DelegateCommand<string>(delegate
+		AppCore.CopyString(lstText);
+		string message = string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_ExtractedLstCount"), count);
+		AppCore.Logger.Success(message);
+		await AppCore.Logger.ShowNotification(new NotificationViewModel<string>(AppSetting.Instance.AppName, message, Res.Instance.VisualStudioBlendLogo2015Pre_16x, AppSetting.Instance.GetIlogger()?.GetStr("mess_ViewDetails"), new DelegateCommand<string>(delegate
 		{
-			AppCore.ShowExtractLstWindow(CS_0024_003C_003E8__locals4.H20qbu03iY);
+			AppCore.ShowExtractLstWindow(lstText);
 		})));
 	}
 
@@ -307,10 +198,10 @@ public class WindowItemCodeSearchViewModel : ViewModelBase
 	{
 		if (e.Item != null && AppCore.ViewModelBase.PVF.PvfIsOpen)
 		{
-			ItemCodeSearchResult itemCodeSearchResult = (ItemCodeSearchResult)e.Item;
-			if (itemCodeSearchResult != null)
+			ItemCodeSearchResult item = (ItemCodeSearchResult)e.Item;
+			if (item != null)
 			{
-				AppCore.ViewModelBase.RootDocument.AddDocument(itemCodeSearchResult.FullPath, gotoNode: true);
+				AppCore.ViewModelBase.RootDocument.AddDocument(item.FullPath, gotoNode: true);
 			}
 		}
 	}
