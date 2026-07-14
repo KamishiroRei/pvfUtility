34053a2171c3cdae2928cfe77fed2b9583888775
Lancarus <duzhen92@qq.com>
2026-07-14 11:55:23 +0800

    fix: restore script comment line generator internals

--- PvfCode/ViewModels/DocumentFolder/VisualLineElementGenerators/ScriptCommentLineGenerator.cs
+++ PvfCode/ViewModels/DocumentFolder/VisualLineElementGenerators/ScriptCommentLineGenerator.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using System.Windows.Input;
-using System.Windows.Threading;
 using ICSharpCode.AvalonEdit.Document;
 using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Rendering;
@@ -17,58 +15,41 @@ namespace PvfCode.ViewModels.DocumentFolder.VisualLineElementGenerators;
 
 public class ScriptCommentLineGenerator : VisualLineElementGenerator, IDisposable
 {
-	[CompilerGenerated]
-	private sealed class _003C_003Ec__DisplayClass15_0
-	{
-		public ScriptCommentLineGenerator xYCexPaKkh;
+	private readonly DocumentHighlighter highlighter;
 
-		public TextSegment LjVeQHP4bs;
+	private readonly TextEditorBase editor;
 
-		public _003C_003Ec__DisplayClass15_0()
-		{
-		}
+	private readonly PvfFile file;
 
-		internal void a7SeGWwhEb()
-		{
-			xYCexPaKkh.Editor.Select(LjVeQHP4bs.StartOffset, LjVeQHP4bs.Length);
-		}
-	}
+	private readonly PvfFileType fileType;
 
-	private readonly DocumentHighlighter BBX5kF7S7Y;
+	private HashSet<string> interestedHighlightingNames;
 
-	private readonly TextEditorBase Editor;
-
-	private readonly PvfFile File;
-
-	private readonly PvfFileType FileType;
-
-	private HashSet<string> Dw350qcChI;
-
-	private readonly FilePathLinkGoToDocumentPathDelegate nBK5722May;
+	private readonly FilePathLinkGoToDocumentPathDelegate goToDocumentPath;
 
 	public ScriptCommentLineGenerator(TextEditorBase editorBase, DocumentHighlighter highlighter, PvfFile file)
 	{
-		Dw350qcChI = new HashSet<string>
+		interestedHighlightingNames = new HashSet<string>
 		{
 			"Section",
 			"SectionEnd",
 			"String"
 		};
-		File = file;
-		FileType = file.FileType;
-		BBX5kF7S7Y = highlighter;
-		Editor = editorBase;
-		nBK5722May = ICD59riq29;
+		this.file = file;
+		fileType = file.FileType;
+		this.highlighter = highlighter;
+		editor = editorBase;
+		goToDocumentPath = GoToDocumentPath;
 	}
 
 	public override VisualLineElement ConstructElement(int offset)
 	{
-		DocumentLine lineByOffset = base.CurrentContext.Document.GetLineByOffset(offset);
-		if (base.CurrentContext.TextView.GetLineIsCollapsed(lineByOffset.LineNumber))
+		DocumentLine line = base.CurrentContext.Document.GetLineByOffset(offset);
+		if (base.CurrentContext.TextView.GetLineIsCollapsed(line.LineNumber))
 		{
 			return null;
 		}
-		HighlightedLine highlightedLine = BBX5kF7S7Y.HighlightLine(lineByOffset.LineNumber);
+		HighlightedLine highlightedLine = highlighter.HighlightLine(line.LineNumber);
 		if (highlightedLine != null && highlightedLine.Sections != null && highlightedLine.Sections.Count > 0)
 		{
 			foreach (HighlightedSection section in highlightedLine.Sections)
@@ -77,10 +58,10 @@ public class ScriptCommentLineGenerator : VisualLineElementGenerator, IDisposabl
 				{
 					continue;
 				}
-				string name = section.Color.Name;
-				if (!(name == "String"))
+				string highlightingName = section.Color.Name;
+				if (highlightingName != "String")
 				{
-					if (!(name == "SectionEnd") && !(name == "Section"))
+					if (highlightingName != "SectionEnd" && highlightingName != "Section")
 					{
 						continue;
 					}
@@ -90,11 +71,11 @@ public class ScriptCommentLineGenerator : VisualLineElementGenerator, IDisposabl
 				{
 					return new ScriptCommentVisualLine(base.CurrentContext.VisualLine, section.Length);
 				}
-				if (AppSetting.Instance.PvfConfig.LstFileUseScriptFile.Contains(File.FileName))
+				if (AppSetting.Instance.PvfConfig.LstFileUseScriptFile.Contains(file.FileName))
 				{
 					return null;
 				}
-				return new FilePathLinkVisualLine(File, nBK5722May, base.CurrentContext.VisualLine, section.Length - 1);
+				return new FilePathLinkVisualLine(file, goToDocumentPath, base.CurrentContext.VisualLine, section.Length - 1);
 			}
 		}
 		return null;
@@ -102,26 +83,26 @@ public class ScriptCommentLineGenerator : VisualLineElementGenerator, IDisposabl
 
 	public override int GetFirstInterestedOffset(int startOffset)
 	{
-		DocumentLine lineByOffset = base.CurrentContext.Document.GetLineByOffset(startOffset);
-		if (base.CurrentContext.TextView.GetLineIsCollapsed(lineByOffset.LineNumber))
+		DocumentLine line = base.CurrentContext.Document.GetLineByOffset(startOffset);
+		if (base.CurrentContext.TextView.GetLineIsCollapsed(line.LineNumber))
 		{
 			return -1;
 		}
-		if (lineByOffset.Length > 0)
+		if (line.Length > 0)
 		{
-			HighlightedLine highlightedLine = BBX5kF7S7Y.HighlightLine(lineByOffset.LineNumber);
+			HighlightedLine highlightedLine = highlighter.HighlightLine(line.LineNumber);
 			if (highlightedLine != null && highlightedLine.Sections != null && highlightedLine.Sections.Count > 0)
 			{
 				foreach (HighlightedSection section in highlightedLine.Sections)
 				{
-					if (section.Offset >= startOffset && Dw350qcChI.Contains(section.Color.Name))
+					if (section.Offset >= startOffset && interestedHighlightingNames.Contains(section.Color.Name))
 					{
-						string name = section.Color.Name;
-						if (name == "String")
+						string highlightingName = section.Color.Name;
+						if (highlightingName == "String")
 						{
 							return section.Offset;
 						}
-						if (name == "SectionEnd" || name == "Section")
+						if (highlightingName == "SectionEnd" || highlightingName == "Section")
 						{
 							return section.Offset;
 						}
@@ -132,9 +113,9 @@ public class ScriptCommentLineGenerator : VisualLineElementGenerator, IDisposabl
 		return -1;
 	}
 
-	private void ICD59riq29(TextSegment P_0, string? fullPath = null)
+	private void GoToDocumentPath(TextSegment segment, string? fullPath = null)
 	{
-		if (((int)Keyboard.Modifiers & 2) != 2)
+		if ((Keyboard.Modifiers & ModifierKeys.Control) != ModifierKeys.Control)
 		{
 			return;
 		}
@@ -145,145 +126,145 @@ public class ScriptCommentLineGenerator : VisualLineElementGenerator, IDisposabl
 				AppCore.ShowMsg(fullPath, isError: true);
 				return;
 			}
-			LAw5PdHnmj(fullPath);
-			Lye5J58Cg0(new TextSegment
+			ScriptGoToFile(fullPath);
+			SelectSegment(new TextSegment
 			{
-				StartOffset = P_0.StartOffset + 1,
-				EndOffset = P_0.EndOffset - 1
+				StartOffset = segment.StartOffset + 1,
+				EndOffset = segment.EndOffset - 1
 			});
 			return;
 		}
-		string text = Editor.Document.GetText(P_0).Replace("`", "");
-		if (string.IsNullOrEmpty(text))
+		string referencedPath = editor.Document.GetText(segment).Replace("`", "");
+		if (string.IsNullOrEmpty(referencedPath))
 		{
 			return;
 		}
-		PvfGroup pVF = AppCore.ViewModelBase.PVF;
-		if (File != null && File.FileType == PvfFileType.lst)
+		PvfGroup pvf = AppCore.ViewModelBase.PVF;
+		if (file != null && file.FileType == PvfFileType.lst)
 		{
-			if (pVF.FindFilePath(text, out string fullPath2))
+			if (pvf.FindFilePath(referencedPath, out string matchedPath))
 			{
-				AppCore.ViewModelBase.RootDocument.AddDocument(fullPath2, gotoNode: true);
+				AppCore.ViewModelBase.RootDocument.AddDocument(matchedPath, gotoNode: true);
 			}
 			else
 			{
-				AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), text));
+				AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), referencedPath));
 			}
 		}
-		else if (FileType == PvfFileType.nut)
+		else if (fileType == PvfFileType.nut)
 		{
-			text = text.Replace("\"", "");
-			if (!string.IsNullOrEmpty(Path.GetExtension(text)))
+			referencedPath = referencedPath.Replace("\"", "");
+			if (!string.IsNullOrEmpty(Path.GetExtension(referencedPath)))
 			{
-				text = text.ToLower().Replace("\\", "/").Replace("../", string.Empty);
-				if (pVF.FileAny(text))
+				referencedPath = referencedPath.ToLower().Replace("\\", "/").Replace("../", string.Empty);
+				if (pvf.FileAny(referencedPath))
 				{
-					AppCore.ViewModelBase.RootDocument.AddDocument(text, gotoNode: true);
+					AppCore.ViewModelBase.RootDocument.AddDocument(referencedPath, gotoNode: true);
 				}
 				else
 				{
-					string text2 = "sqr/" + text;
-					string fullPath3;
-					if (pVF.FileAny(text2))
+					string sqrPath = "sqr/" + referencedPath;
+					string matchedPath;
+					if (pvf.FileAny(sqrPath))
 					{
-						text = text2;
-						AppCore.ViewModelBase.RootDocument.AddDocument(text, gotoNode: true);
+						referencedPath = sqrPath;
+						AppCore.ViewModelBase.RootDocument.AddDocument(referencedPath, gotoNode: true);
 					}
-					else if (pVF.FindFilePath(text, out fullPath3))
+					else if (pvf.FindFilePath(referencedPath, out matchedPath))
 					{
-						AppCore.ViewModelBase.RootDocument.AddDocument(fullPath3, gotoNode: true);
+						AppCore.ViewModelBase.RootDocument.AddDocument(matchedPath, gotoNode: true);
 					}
 					else
 					{
-						AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), text));
+						AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), referencedPath));
 					}
 				}
 			}
 		}
 		else
 		{
-			LAw5PdHnmj(text);
+			ScriptGoToFile(referencedPath);
 		}
-		Lye5J58Cg0(new TextSegment
+		SelectSegment(new TextSegment
 		{
-			StartOffset = P_0.StartOffset + 1,
-			EndOffset = P_0.EndOffset - 1
+			StartOffset = segment.StartOffset + 1,
+			EndOffset = segment.EndOffset - 1
 		});
 	}
 
-	private void LAw5PdHnmj(string P_0)
+	private void ScriptGoToFile(string filePath)
 	{
 		try
 		{
-			if (File == null)
+			if (file == null)
 			{
 				return;
 			}
-			P_0 = P_0.Replace("\\", "/").ToLower();
-			PvfGroup pVF = AppCore.ViewModelBase.PVF;
-			string text = Path.GetExtension(P_0).ToLower();
-			if (text != ".img")
+			filePath = filePath.Replace("\\", "/").ToLower();
+			PvfGroup pvf = AppCore.ViewModelBase.PVF;
+			string extension = Path.GetExtension(filePath).ToLower();
+			if (extension != ".img")
 			{
-				string filePath = P_0;
-				if (pVF.FileAny(filePath))
+				string candidatePath = filePath;
+				if (pvf.FileAny(candidatePath))
 				{
-					AppCore.ViewModelBase.RootDocument.AddDocument(filePath, gotoNode: true);
+					AppCore.ViewModelBase.RootDocument.AddDocument(candidatePath, gotoNode: true);
 					return;
 				}
-				filePath = Path.Combine(Path.GetDirectoryName(File.FileName), P_0).ToLower().Replace("\\", "/");
-				if (pVF.FileAny(filePath))
+				candidatePath = Path.Combine(Path.GetDirectoryName(file.FileName), filePath).ToLower().Replace("\\", "/");
+				if (pvf.FileAny(candidatePath))
 				{
-					AppCore.ViewModelBase.RootDocument.AddDocument(filePath, gotoNode: true);
+					AppCore.ViewModelBase.RootDocument.AddDocument(candidatePath, gotoNode: true);
 				}
-				else if (pVF.FindFilePath(P_0, out filePath))
+				else if (pvf.FindFilePath(filePath, out candidatePath))
 				{
-					AppCore.ViewModelBase.RootDocument.AddDocument(filePath, gotoNode: true);
+					AppCore.ViewModelBase.RootDocument.AddDocument(candidatePath, gotoNode: true);
 				}
-				else if (P_0.Contains("../"))
+				else if (filePath.Contains("../"))
 				{
-					int count = new Regex("\\.\\./", RegexOptions.Compiled).Matches(P_0).Count;
-					string directoryName = Path.GetDirectoryName(File.FileName);
-					for (int i = 0; i < count; i++)
+					int parentDirectoryCount = new Regex("\\.\\./", RegexOptions.Compiled).Matches(filePath).Count;
+					string directoryName = Path.GetDirectoryName(file.FileName);
+					for (int i = 0; i < parentDirectoryCount; i++)
 					{
 						if (string.IsNullOrEmpty(directoryName))
 						{
-							AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), P_0));
+							AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), filePath));
 							return;
 						}
 						directoryName = Path.GetDirectoryName(directoryName);
 					}
-					string text2 = P_0.Replace("../", "");
-					string text3 = ((!string.IsNullOrEmpty(directoryName)) ? Path.Combine(directoryName, text2).Replace("\\", "/").ToLower() : text2);
-					if (pVF.FileAny(text3))
+					string relativePath = filePath.Replace("../", "");
+					string resolvedPath = ((!string.IsNullOrEmpty(directoryName)) ? Path.Combine(directoryName, relativePath).Replace("\\", "/").ToLower() : relativePath);
+					if (pvf.FileAny(resolvedPath))
 					{
-						AppCore.ViewModelBase.RootDocument.AddDocument(text3, gotoNode: true);
+						AppCore.ViewModelBase.RootDocument.AddDocument(resolvedPath, gotoNode: true);
 					}
 					else
 					{
-						AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), text3));
+						AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), resolvedPath));
 					}
 				}
-				else if (FileType == PvfFileType.twn && text == ".map")
+				else if (fileType == PvfFileType.twn && extension == ".map")
 				{
-					string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(P_0);
-					string filePath2 = "map/" + P_0.Replace("/" + fileNameWithoutExtension, "/(r)" + fileNameWithoutExtension);
-					if (pVF.FileAny(filePath2))
+					string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+					string mapPath = "map/" + filePath.Replace("/" + fileNameWithoutExtension, "/(r)" + fileNameWithoutExtension);
+					if (pvf.FileAny(mapPath))
 					{
-						AppCore.ViewModelBase.RootDocument.AddDocument(filePath2, gotoNode: true);
+						AppCore.ViewModelBase.RootDocument.AddDocument(mapPath, gotoNode: true);
 					}
 					else
 					{
-						AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), P_0));
+						AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), filePath));
 					}
 				}
 				else
 				{
-					AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), P_0));
+					AppCore.Logger.ShowMsg(string.Format(AppSetting.Instance.GetIlogger()?.GetStr("mess_FileNotExist"), filePath));
 				}
 			}
-			else if (text.ToLower() == ".img")
+			else if (extension.ToLower() == ".img")
 			{
-				ihT5ZDkX12(P_0.Replace("`", "").ToLower());
+				OpenImagePackFile(filePath.Replace("`", "").ToLower());
 			}
 		}
 		catch (Exception e)
@@ -292,24 +273,21 @@ public class ScriptCommentLineGenerator : VisualLineElementGenerator, IDisposabl
 		}
 	}
 
-	private void ihT5ZDkX12(string P_0)
+	private void OpenImagePackFile(string filePath)
 	{
-		ResultData resultData = ImagePack2Service.Instance.ImgGoToNpkFile(P_0.Replace("%04d", "0001").Replace("%02d%02d", "0001"));
-		if (resultData.IsError)
+		ResultData result = ImagePack2Service.Instance.ImgGoToNpkFile(filePath.Replace("%04d", "0001").Replace("%02d%02d", "0001"));
+		if (result.IsError)
 		{
-			AppCore.ShowMsg(resultData.Msg, isError: true);
+			AppCore.ShowMsg(result.Msg, isError: true);
 		}
 	}
 
-	private void Lye5J58Cg0(TextSegment P_0)
+	private void SelectSegment(TextSegment segment)
 	{
-		_003C_003Ec__DisplayClass15_0 CS_0024_003C_003E8__locals5 = new _003C_003Ec__DisplayClass15_0();
-		CS_0024_003C_003E8__locals5.xYCexPaKkh = this;
-		CS_0024_003C_003E8__locals5.LjVeQHP4bs = P_0;
-		((DispatcherObject)Editor).Dispatcher.BeginInvoke((Delegate)(Action)delegate
+		editor.Dispatcher.BeginInvoke((Action)delegate
 		{
-			CS_0024_003C_003E8__locals5.xYCexPaKkh.Editor.Select(CS_0024_003C_003E8__locals5.LjVeQHP4bs.StartOffset, CS_0024_003C_003E8__locals5.LjVeQHP4bs.Length);
-		}, Array.Empty<object>());
+			editor.Select(segment.StartOffset, segment.Length);
+		});
 	}
 
 	public void Dispose()
